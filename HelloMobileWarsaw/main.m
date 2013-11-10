8192663b439c612a4ceab416737e189174619bc5
Pawel Dudek <pawel@dudek.mobi>
2013-11-10 19:41:59 +0100

    Initial Commit

---
+++ HelloMobileWarsaw/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloMobileWarsaw
+//
+//  Created by Pawel Dudek on 10/11/13.
+//  Copyright (c) 2013 Pawel Dudek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
